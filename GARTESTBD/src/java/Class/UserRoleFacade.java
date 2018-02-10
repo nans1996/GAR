60b11857ab3a8ed837f91f0e674a28ba68cd6e17
Vasilisa Lapygina <vasilek_l@inbox.ru>
2018-02-10 19:04:53 +0300

    Тестовый проект
    Обнаружено id не инкремент

---
+++ GARTESTBD/src/java/Class/UserRoleFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Vasilisa
+ */
+@Stateless
+public class UserRoleFacade extends AbstractFacade<UserRole> {
+
+    @PersistenceContext(unitName = "GARTESTBDPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UserRoleFacade() {
+        super(UserRole.class);
+    }
+    
+}
