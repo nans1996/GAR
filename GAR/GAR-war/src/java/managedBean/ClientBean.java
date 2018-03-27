2ba76c0643a638e1af89bd720ab3a4ad3f80c435
nans1996 <nasty.rod@yandex.ru>
2018-03-27 23:16:27 +0300

    beans

--- GAR/GAR-war/src/java/managedBean/ClientBean.java
+++ GAR/GAR-war/src/java/managedBean/ClientBean.java
@@ -20,7 +20,21 @@ import entitys.*;
 @Named(value = "clientBean")
 @SessionScoped
 public class ClientBean implements Serializable {
+
+    @EJB
+    private TopicFacadeLocal topicFacade;
+Topic t = new Topic();
+    @EJB
+    private MessageFacadeLocal messageFacade;
+    Message m = new Message();
  //пробуем написать контроллер
+    @EJB
+    private GoalUserFacadeLocal goalUserFacade;
+GoalUser gu = new GoalUser();
+
+    
+    
+    
     Client c = new Client();
     @EJB
     private ClientFacadeLocal clientFacade;
@@ -54,5 +68,44 @@ public class ClientBean implements Serializable {
        this.clientFacade.edit(this.c);
        this.c = new Client();
        return "index";
+   }
+   
+    //вывести цели клиентов
+   public List<GoalUser> findAllGoalClient(){
+      return this.goalUserFacade.findAll();
+   }
+   //создать 
+   public String createGoalUser(){
+       this.goalUserFacade.create(this.gu);
+       //после добавления перебрасывает на index
+     return "index";
+   }
+    //удалить
+   public void deleteGoalUser(GoalUser gu){
+       this.goalUserFacade.remove(gu);
+   }
+   
+   //обновить 
+   public String editGoalUser(GoalUser gu){
+       this.gu = gu;
+       return "edit";
+   }
+   public String editGoalUser(){
+       this.goalUserFacade.edit(this.gu);
+       this.gu = new GoalUser();
+       return "index";
+   }
+   //создать сообщение пока так же
+    public String createMessage(){
+       this.messageFacade.create(this.m);
+       //после добавления перебрасывает на index
+     return "index";
+   }
+    
+    //создать новую тему на форуме
+    public String createTopic(){
+       this.topicFacade.create(this.t);
+       //после добавления перебрасывает на index
+     return "index";
    }
 }
--- GAR/GAR-war/src/java/managedBean/UserBean.java
+++ GAR/GAR-war/src/java/managedBean/UserBean.java
@@ -6,10 +6,13 @@
 package managedBean;
 
 //import entitys.User;
+import entitys.*;
 import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import model.UserFacadeLocal;
 import org.apache.myfaces.trinidad.component.UIXTable;
 
 /**
@@ -20,13 +23,39 @@ import org.apache.myfaces.trinidad.component.UIXTable;
 @ManagedBean(name = "userBean")
 @RequestScoped
 public class UserBean {
+
+    @EJB
+    private UserFacadeLocal userFacade;
+    User u = new User();
     
-   // public UserBean(){
-    //    user = new User();
-   // }
+    public UserBean(){
+        
+    }
     
-   // private User user;
-    //впринципе не обязательно использовать тринидад, можно написать в стиле .NET
-    protected UIXTable table;
-    
+     //вывести всех пользователей
+   public List<User> findAllUser(){
+      return this.userFacade.findAll();
+   }
+   //создать  
+   public String createUser(){
+       this.userFacade.create(this.u);
+       //после добавления перебрасывает на index
+     return "index";
+   }
+    //удалить
+   public void deleteUser(User u){
+       this.userFacade.remove(u);
+   }
+   
+   //обновить 
+   public String editUser(User u){
+       this.u = u;
+       return "edit";
+   }
+   public String editUser(){
+       this.userFacade.edit(this.u);
+       this.u = new User();
+       return "index";
+   }
+
 }
