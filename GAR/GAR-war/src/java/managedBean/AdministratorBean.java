7ff253321ec56e32dbcd56a74b26a3710885e6b3
Vasilisa Lapygina <vasilek_l@inbox.ru>
2018-05-01 14:13:42 +0300

    Метод для Бина для добавления картинки

--- GAR/GAR-war/src/java/managedBean/AdministratorBean.java
+++ GAR/GAR-war/src/java/managedBean/AdministratorBean.java
@@ -9,10 +9,13 @@ import entity.User;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import org.apache.myfaces.trinidad.component.UIXTable;
 import model.*;
+import org.primefaces.event.FileUploadEvent;
 /**
  *
  * @author Vasilisa
@@ -76,6 +79,9 @@ public class AdministratorBean {
         this.user = user;
     }
     
-    
+    public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
     
 }
