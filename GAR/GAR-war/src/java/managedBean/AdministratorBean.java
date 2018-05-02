1317bddccf6d8ec52fdf95c5da80301fe9458b14
Vasilisa Lapygina <vasilek_l@inbox.ru>
2018-05-02 19:28:36 +0300

    Табличка в полосочку

--- GAR/GAR-war/src/java/managedBean/AdministratorBean.java
+++ GAR/GAR-war/src/java/managedBean/AdministratorBean.java
@@ -11,7 +11,10 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import model.*;
 import entity.*;
+import java.io.IOException;
+import java.io.InputStream;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import org.primefaces.event.FileUploadEvent;
@@ -21,6 +24,7 @@ import org.primefaces.model.UploadedFile;
  * @author Vasilisa
  */
 @ManagedBean(name = "administratorBean")
+@RequestScoped
 public class AdministratorBean {
     public AdministratorBean() {
         //user = new User();
@@ -37,13 +41,10 @@ public class AdministratorBean {
     Image image = new Image() ;
     
 //    Администрирование пользователей
-    public List<User> getAll() {
+    public List<User> getAllUser() {
         return  userFacadeLocal.findAll();
     }
-        //вывести всех пользователей
-    public List<User> findAll() {
-        return this.userFacade.findAll();
-    }
+    
     //создать  
 
     public String createUser() {
@@ -71,16 +72,38 @@ public class AdministratorBean {
         this.user = user;
     }
     
-        public void handleFileUpload(FileUploadEvent event) {
+    public void handleFileUpload(FileUploadEvent event) {
         UploadedFile uploadedFile = event.getFile();
         image.setName(uploadedFile.getFileName());
         image.setPersonageImageCollection(null);
         image.setType(uploadedFile.getContentType());
         image.setData(uploadedFile.getContents());
         imageFacadeLocal.create(image);
-        
         FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
         FacesContext.getCurrentInstance().addMessage(null, message);
-       
-    }    
+    }
+ 
+    private UploadedFile file;
+
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+
+    public void upload() throws IOException {
+        if (file != null) {
+            image.setName(file.getFileName());
+            image.setPersonageImageCollection(null);
+            image.setType(file.getContentType());
+            InputStream in = file.getInputstream();
+            image.setData(file.getContents());
+            imageFacadeLocal.create(image);
+            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+        
+    }
 }
